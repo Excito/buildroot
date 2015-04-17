1756138e25f36514fda42dfc1a7e79e4a7e3dc17
Charles leclerc <leclerc.charles@gmail.com>
2015-04-17 15:51:48 -0400

    Added cdebootstrap package

---
+++ package/cdebootstrap/cdebootstrap.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# cdebootstrap
+#
+################################################################################
+
+CDEBOOTSTRAP_VERSION = 0.6.4
+CDEBOOTSTRAP_SOURCE = cdebootstrap_$(CDEBOOTSTRAP_VERSION).tar.gz
+CDEBOOTSTRAP_SITE = http://ftp.de.debian.org/debian/pool/main/c/cdebootstrap/
+CDEBOOTSTRAP_LICENSE_FILE = debian/copyright
+CDEBOOTSTRAP_DEPENDENCIES = bzip2 xz zlib libdebian-installer
+
+$(eval $(autotools-package))
