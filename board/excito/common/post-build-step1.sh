fd806e16ed54fe9f79e8f2b1a34da98cd2369ce2
Charles leclerc <leclerc.charles@gmail.com>
2015-09-06 20:41:39 -0400

    Ported new 2-step configuration to the Bubba|2 + small fixes

--- board/excito/common/post-build-step1.sh
+++ board/excito/common/post-build-step1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p $BASE_DIR/excito
+[ -e $BASE_DIR/excito/modules ] && rm -rf $BASE_DIR/excito/modules
 mv $TARGET_DIR/lib/modules $BASE_DIR/excito
 rm -f $TARGET_DIR/lib/libgcc_s.so $TARGET_DIR/lib/libgcc_s.so.1
-rm -rf $TARGET_DIR/etc/{hostname,hosts,issue,network,os-release,ld.so.conf}
+rm -rf $TARGET_DIR/etc/{hostname,issue,network,os-release,ld.so.conf}
