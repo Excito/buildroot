76019aa6d009e0165cc52a13c53bf21196de1a1a
Charles Leclerc <leclerc.charles@gmail.com>
2015-12-03 08:59:19 -0500

    Integrated excito-installer package

---
+++ package/excito-installer/excito-installer.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# excito-installer
+#
+################################################################################
+
+EXCITO_INSTALLER_VERSION = 4d14adb
+EXCITO_INSTALLER_SITE = $(call github,Excito,installer,$(EXCITO_INSTALLER_VERSION))
+
+define EXCITO_INSTALLER_INSTALL_TARGET_CMDS
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/opt/excito
+	$(INSTALL) -m 0644 $(@D)/*.py $(TARGET_DIR)/opt/excito
+	$(INSTALL) -m 0755 $(@D)/installer $(TARGET_DIR)/opt/excito
+endef
+
+$(eval $(generic-package))
