6d9c7fa1465deae408cb5fd86bb70a0b5b247fc8
Charles Leclerc <leclerc.charles@gmail.com>
2015-12-04 15:57:29 -0500

    Released version 1.1 with excito-installer 1.0

--- package/excito-installer/excito-installer.mk
+++ package/excito-installer/excito-installer.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXCITO_INSTALLER_VERSION = eb2aabc
+EXCITO_INSTALLER_VERSION = 1.0
 EXCITO_INSTALLER_SITE = $(call github,Excito,installer,$(EXCITO_INSTALLER_VERSION))
 
 define EXCITO_INSTALLER_INSTALL_TARGET_CMDS
