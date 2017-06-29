7d13633cd0722da0b2cab21d26de66338b2e21f5
Charles Leclerc <leclerc.charles@gmail.com>
2017-06-29 10:27:12 -0400

    Ported debian-archive-keyring

--- package/debian-archive-keyring/debian-archive-keyring.mk
+++ package/debian-archive-keyring/debian-archive-keyring.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-DEBIAN_ARCHIVE_KEYRING_VERSION = 2014.3
+DEBIAN_ARCHIVE_KEYRING_VERSION = 2017.5
 DEBIAN_ARCHIVE_KEYRING_SOURCE = debian-archive-keyring_$(DEBIAN_ARCHIVE_KEYRING_VERSION).tar.xz
-DEBIAN_ARCHIVE_KEYRING_SITE = http://ftp.de.debian.org/debian/pool/main/d/debian-archive-keyring
+DEBIAN_ARCHIVE_KEYRING_SITE = http://http.debian.net/debian/pool/main/d/debian-archive-keyring
 DEBIAN_ARCHIVE_KEYRING_LICENSE_FILES = debian/copyright
-DEBIAN_ARCHIVE_KEYRING_DEPENDENCIES = host-jetring host-gnupg
+DEBIAN_ARCHIVE_KEYRING_DEPENDENCIES = host-jetring
 
 define DEBIAN_ARCHIVE_KEYRING_INSTALL_TARGET_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE1) -C $(@D) DESTDIR=$(TARGET_DIR) install
