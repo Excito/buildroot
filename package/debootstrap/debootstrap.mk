1f517b3f003feccaaebabac98caa270df0e46efe
Charles Leclerc <leclerc.charles@gmail.com>
2017-06-29 14:58:49 -0400

    re-added pkgdetails.c into debootstrap

--- package/debootstrap/debootstrap.mk
+++ package/debootstrap/debootstrap.mk
@@ -9,6 +9,10 @@ DEBOOTSTRAP_SOURCE = debootstrap_$(DEBOOTSTRAP_VERSION).tar.gz
 DEBOOTSTRAP_SITE = http://http.debian.net/debian/pool/main/d/debootstrap
 DEBOOTSTRAP_LICENSE_FILE = debian/copyright
 
+define DEBOOTSTRAP_BUILD_CMDS
+	CC="$(TARGET_CC)" $(MAKE) -C $(@D)
+endef
+
 define DEBOOTSTRAP_INSTALL_TARGET_CMDS
 	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) install
 endef
