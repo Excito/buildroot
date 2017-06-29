3b63beeb5daffa9634d8f23002a890da1ecdb91b
Charles Leclerc <leclerc.charles@gmail.com>
2017-06-29 09:29:33 -0400

    Updated libdebian-installer

--- package/libdebian-installer/libdebian-installer.mk
+++ package/libdebian-installer/libdebian-installer.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-LIBDEBIAN_INSTALLER_VERSION = 0.99
+LIBDEBIAN_INSTALLER_VERSION = 0.110
 LIBDEBIAN_INSTALLER_SOURCE = libdebian-installer_$(LIBDEBIAN_INSTALLER_VERSION).tar.xz
-LIBDEBIAN_INSTALLER_SITE = http://ftp.de.debian.org/debian/pool/main/libd/libdebian-installer
+LIBDEBIAN_INSTALLER_SITE = http://http.debian.net/debian/pool/main/libd/libdebian-installer
 LIBDEBIAN_INSTALLER_LICENSE_FILE = debian/copyright
+LIBDEBIAN_INSTALLER_DEPENDENCIES = check
 LIBDEBIAN_INSTALLER_AUTORECONF = YES
 LIBDEBIAN_INSTALLER_INSTALL_STAGING = YES
 
