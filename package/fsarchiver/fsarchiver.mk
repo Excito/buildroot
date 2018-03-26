18f760079e5deef9387e3e06b2a5edc2a27fd4f3
Charles Leclerc <leclerc.charles@gmail.com>
2018-03-26 23:02:07 +0000

    Enabled LZO for fsarchiver

--- package/fsarchiver/fsarchiver.mk
+++ package/fsarchiver/fsarchiver.mk
@@ -8,8 +8,7 @@ FSARCHIVER_VERSION = 0.8.1
 FSARCHIVER_SOURCE = fsarchiver-$(FSARCHIVER_VERSION).tar.gz
 FSARCHIVER_SITE = $(call github,fdupoux,fsarchiver,$(FSARCHIVER_VERSION))
 FSARCHIVER_LICENSE_FILE = COPYING
-FSARCHIVER_DEPENDENCIES = attr zlib bzip2 libgcrypt util-linux e2fsprogs xz
+FSARCHIVER_DEPENDENCIES = attr zlib bzip2 libgcrypt util-linux e2fsprogs xz lzo
 FSARCHIVER_AUTORECONF = YES
-FSARCHIVER_CONF_OPTS = --disable-lzo
 
 $(eval $(autotools-package))
