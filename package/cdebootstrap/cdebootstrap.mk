6d1da3ea966ff4f41e590a5b709b661507cc972a
Charles Leclerc <leclerc.charles@gmail.com>
2020-03-24 15:36:04 -0400

    More packages fixes

--- package/cdebootstrap/cdebootstrap.mk
+++ package/cdebootstrap/cdebootstrap.mk
@@ -8,7 +8,7 @@ CDEBOOTSTRAP_VERSION = 0.7.7
 CDEBOOTSTRAP_SOURCE = cdebootstrap_$(CDEBOOTSTRAP_VERSION).tar.xz
 CDEBOOTSTRAP_SITE = http://http.debian.net/debian/pool/main/c/cdebootstrap
 CDEBOOTSTRAP_LICENSE_FILE = debian/copyright
-CDEBOOTSTRAP_DEPENDENCIES = bzip2 libcurl xz zlib debian-archive-keyring libdebian-installer
+CDEBOOTSTRAP_DEPENDENCIES = bzip2 libcurl xz zlib debian-archive-keyring libdebian-installer host-dpkg-deb
 CDEBOOTSTRAP_AUTORECONF = YES
 CDEBOOTSTRAP_CONF_OPTS = --with-frontend=standalone
 
--- package/fsarchiver/fsarchiver.mk
+++ package/fsarchiver/fsarchiver.mk
@@ -8,7 +8,7 @@ FSARCHIVER_VERSION = 0.8.5
 FSARCHIVER_SOURCE = fsarchiver-$(FSARCHIVER_VERSION).tar.gz
 FSARCHIVER_SITE = $(call github,fdupoux,fsarchiver,$(FSARCHIVER_VERSION))
 FSARCHIVER_LICENSE_FILE = COPYING
-FSARCHIVER_DEPENDENCIES = attr zlib bzip2 libgcrypt util-linux e2fsprogs xz lzo
+FSARCHIVER_DEPENDENCIES = attr zlib bzip2 libgcrypt util-linux e2fsprogs xz lzo lz4 zstd
 FSARCHIVER_AUTORECONF = YES
 
 $(eval $(autotools-package))
